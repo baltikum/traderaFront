bab0c3d15ba1787ea0b7a1f3e6084fc0e1228f2e
baltikum <mwdavidsson@outlook.com>
2023-04-20 11:04:05 +0200

    ignore

--- src/components/data/ItemObject.js
+++ src/components/data/ItemObject.js
@@ -1,13 +1,25 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
-import { Button } from "react-bootstrap";
+//import { Button } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart, faFlag } from '@fortawesome/free-solid-svg-icons'
 import CategoryStringConverter from './components/CategoryStringConverter';
 
 const ItemObject = (props) => {
+  const fetchBiddingData = () => {
+    axios.get('http://127.0.0.1:8000/get_bidding/'+ props.item.id)
+      .then( response => {
+        console.log(response.data);
+      })
+      .catch(error => console.log(error));
+  };
+  
+  const [bidding, setBidding] = useState(Boolean(props.item.bidding_on));
+  const [bid,setBid] = useState(bidding ? fetchBiddingData() : 0);
   const [highlighted, setHighlighted] = useState(Boolean(props.item.highlighted));
   const [removed, setRemoved] = useState(Boolean(props.item.removed));
   const [ending_time, setEnding] = useState('')
@@ -16,6 +28,21 @@ const ItemObject = (props) => {
   const handleHighlight = () => {
     setHighlighted(!highlighted);
     axios.patch('http://127.0.0.1:8000/highlight_auction/'+ props.item.id)
+      .then( response => {
+        console.log(response.data);
+      })
+      .catch(error => console.log(error));
+  }
+  const handleBid = () => {
+
+    setBidding(!bidding);
+    const postData = {
+      auction_id: props.item.id,
+      highest_bid: bid,
+      ends: props.item.end_date
+    };
+
+    axios.post('http://127.0.0.1:8000/insert_bid/'+ props.item.id, postData)
       .then( response => {
         console.log(response.data);
       })
@@ -36,6 +63,7 @@ const ItemObject = (props) => {
   };
 
 
+  
 
   useEffect(() => {
     const datetime = new Date(props.item.end_date);
@@ -54,18 +82,13 @@ const ItemObject = (props) => {
   },[]);
 
   const [menuVisible, setMenuVisible] = useState(false);
-  const [menuX, setMenuX] = useState(0);
-  const [menuY, setMenuY] = useState(0);
 
   function handleContextMenu(event) {
     event.preventDefault();
     setMenuVisible(!menuVisible);
   }
 
-  function handleMenuClick() {
-    setMenuVisible(false);
-    // Handle menu item click here
-  }
+  
 
   return (
     <Card {...props} 
@@ -80,6 +103,9 @@ const ItemObject = (props) => {
                   : 'card text-white bg-info mb-3'
           }
         >
+
+
+
       <Card.Img 
         variant="top" 
         src={props.item.image_url} 
@@ -89,36 +115,67 @@ const ItemObject = (props) => {
           handleRemove();
         }}
         />
+      { menuVisible ? 
+      
+      
+      <>
+        <Form>
+          <Form.Group className="mb-3" 
+                      controlId="formBid" 
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        handleBid();
+                      }}>
+            <Form.Label>Max bud</Form.Label>
+            <Form.Control type="number"  
+                          placeholder="Enter bid" 
+                          onChange={(e) => setBid(e.target.value)}/>
 
-      <Card.Body>
-        { highlighted ? <FontAwesomeIcon icon={faHeart} size="lg" color="red" /> : <FontAwesomeIcon icon={faFlag} size="lg" color="yellow" /> }
-        { menuVisible ? 
-        <><Card.Title>{props.item.description.slice(0,13)}</Card.Title>
-        <Card.Text>{props.item.description}</Card.Text>
-        <Card.Text>{props.item.price}</Card.Text> </>: null }
-        
-        <Button 
-            className="text-capitalize bg-warning"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleHighlight(); }}> 
-              { highlighted ? 'Släpp' : 'Spara' }
-        </Button>
-        
-        <Button 
-            className="text-capitalize bg-danger"
-            onClick={(e) => {
-              e.stopPropagation();
-              handleRemove();
-            }}>
-              { removed ? 'Återställ' : 'Ta Bort' }
-        </Button>
+          </Form.Group>
+          <Button variant="warning" 
+                  type="submit">   
+            Buda
+          </Button>
+        </Form>
 
-        <Card.Text>{props.item.search_term} in &nbsp;
-          <CategoryStringConverter number={props.item.auction_category} />
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer>{ending_time}</Card.Footer>
+      </>
+
+
+        : <>
+        <Card.Body>
+          { highlighted ? <FontAwesomeIcon icon={faHeart} size="lg" color="red" /> : <FontAwesomeIcon icon={faFlag} size="lg" color="yellow" /> }
+          
+          <Card.Title>{props.item.description.slice(0,13)}</Card.Title>
+          <Card.Text>{props.item.description}</Card.Text>
+          <Card.Text>{props.item.price}</Card.Text> 
+          
+          <Button 
+              className="text-capitalize bg-warning"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleHighlight(); }}> 
+                { highlighted ? 'Släpp' : 'Spara' }
+          </Button>
+          
+          <Button 
+              className="text-capitalize bg-danger"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleRemove();
+              }}>
+                { removed ? 'Återställ' : 'Ta Bort' }
+          </Button>
+
+          <Card.Text>{props.item.search_term} in &nbsp;
+            <CategoryStringConverter number={props.item.auction_category} />
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>{ending_time}</Card.Footer>
+      </> }
+
+
+
+      
     </Card>
   );
 };
